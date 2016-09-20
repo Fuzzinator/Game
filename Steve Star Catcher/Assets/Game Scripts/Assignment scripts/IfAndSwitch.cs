699a03f530572567dfd7b5854216316f5add8383
Ben <Fuzzinator12@gmail.com>
2016-09-19 23:16:43 -0600

    If and switch statements

---
+++ Steve Star Catcher/Assets/Game Scripts/Assignment scripts/IfAndSwitch.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IfAndSwitch: MonoBehaviour {
+
+    public float time;
+    public int sleepHours;
+    private int youGetSleep;
+
+
+	// Use this for initialization
+	void Start () {
+        time = 12;
+        sleepHours = 3;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+
+        if (sleepHours <= time)
+
+        {
+            youGetSleep = Random.Range(1, 5);
+            switch (youGetSleep)
+            {
+                case 1:
+                    print("You get to sleep");
+                    break;
+                case 2:
+                    print("Congratulations! Sleep is your lady tonight!");
+                    break;
+                case 3:
+                    print("Luck be sleeping tonight!");
+                    break;
+                case 4:
+                    print("Boy you best go take a nap");
+                    break;
+                case 5:
+                    print("AWWW YEAAAH SLEEP TIME!");
+                    break;
+
+            }
+
+
+        }
+             
+	
+	}
+}
