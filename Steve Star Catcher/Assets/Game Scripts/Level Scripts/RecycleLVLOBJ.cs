93922b62a8f2daa342160e36b042ff1fa05ceba1
Ben <Fuzzinator12@gmail.com>
2016-09-23 22:23:42 -0600

    List

--- Steve Star Catcher/Assets/Game Scripts/Level Scripts/RecycleLVLOBJ.cs
+++ Steve Star Catcher/Assets/Game Scripts/Level Scripts/RecycleLVLOBJ.cs
@@ -1,18 +1,49 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using System;
 
 public class RecycleLVLOBJ : MonoBehaviour {
 
     private Vector3 newLocation;
 
+    System.Random rnd = new System.Random();
+
+    public List<Recycler> recyclableList;
+
+    private int nextTerrain;
+
+    private int i;
+
+    void Start()
+    {
+        recyclableList = new List<Recycler>();
+        Recycler.RecycleAction += RecycleActionHandler;
+
+    }
+
+    private void RecycleActionHandler(Recycler _recycler)
+    {
+        recyclableList.Add(_recycler);
+    }
+
     void OnTriggerEnter()
     {
+        nextTerrain = rnd.Next(1, recyclableList.Count - 1);
 
         newLocation.x = StaticVars.nextSectionPos;
 
-        transform.position = newLocation;
+        recyclableList[nextTerrain].cube.position = newLocation;
+       
 
         StaticVars.nextSectionPos += StaticVars.distance;
+       
+     
+        if(recyclableList.Count > 0)
+            recyclableList.RemoveAt(nextTerrain);
+
 
     }
+
+    
 }
