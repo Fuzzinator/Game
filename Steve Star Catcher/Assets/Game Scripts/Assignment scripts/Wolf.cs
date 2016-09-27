c5dfad9195b54f720422e6dd37aef4f5d496b82e
Ben <Fuzzinator12@gmail.com>
2016-09-26 22:34:16 -0600

    Inheritance

---
+++ Steve Star Catcher/Assets/Game Scripts/Assignment scripts/Wolf.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wolf : Enemies {
+
+    void start()
+    {
+        declareStuff();
+    }
+
+
+	void Update()
+    {
+        hurtPlayer();
+    }
+         
+}
