b86ae7988fe35e1ddd10d1e33ee92ab0e15a24cb
Ben <Fuzzinator12@gmail.com>
2016-11-10 12:19:45 -0700

    wolf hurts player

---
+++ Steve Star Catcher/Assets/Game Scripts/HurtPlayer/WolfHurtPlayer.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WolfHurtPlayer : MonoBehaviour {
+
+    public GameObject hare;
+    public GameObject damageCol;
+    CharacterController cc;
+    public float speed = -80;
+    private Vector3 kickBack;
+
+    //public int wolfDamage = 5;    
+
+    IEnumerator FrontBlowBack()
+    {
+        
+        int temp = 50;
+        damageCol.SetActive(false);
+        MovePlayer.hurt = true;
+        //DestroyStar.canCollect = false;
+       
+        if (temp > 0)
+        {
+            
+            kickBack = new Vector3(speed, -speed, 0);
+            cc = hare.GetComponent<CharacterController>();
+            cc.Move(kickBack * Time.deltaTime);
+            yield return new WaitForSeconds(01f);
+            temp--;
+
+        }
+        
+        yield return new WaitForSeconds(3);
+        damageCol.SetActive(true);
+        MovePlayer.hurt = false;
+        //DestroyStar.canCollect = true;
+    }
+
+
+
+	void OnTriggerEnter(Collider col)
+    {
+
+        if (col.name == "KnockBackFront")
+        {
+            StaticVars.playerScore = 0;
+
+            //print(StaticVars.playerScore);
+            //if (StaticVars.playerScore >= wolfDamage && MovePlayer.hurt == false)
+            //{
+            //    //print("lose 5 stars");
+            //    StaticVars.playerScore -= wolfDamage; //TODO ADD IN POINT REDUCTION\
+            //    print(StaticVars.playerScore);
+            //}
+            //if (StaticVars.playerScore < wolfDamage && MovePlayer.hurt == false)
+            //{
+            //    StaticVars.playerScore = 0;
+            //    print("lose " + StaticVars.playerScore + "stars");
+            //}
+
+            StartCoroutine(FrontBlowBack());
+
+        }
+
+    }
+	
+}
