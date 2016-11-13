1bd20ed4574efc5793d40cf6f8c6d8d24dddfcc3
Ben <Fuzzinator12@gmail.com>
2016-11-12 21:15:04 -0700

    Updated graphics
    menu buttons changed as well as bring it on button

---
+++ Steve Star Catcher/Assets/UI/CloseMenu.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CloseMenu : MonoBehaviour {
+
+    public GameObject menuWindow;
+
+
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+
+            menuWindow.SetActive(false);
+
+        }
+    }
+}
