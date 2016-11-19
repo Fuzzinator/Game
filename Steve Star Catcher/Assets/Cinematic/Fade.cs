e0933d29f29ada44dd82b07a197fa0357078e929
Ben <Fuzzinator12@gmail.com>
2016-11-18 22:57:37 -0700

    game intro added
    watercolor painting intro added before game menu

---
+++ Steve Star Catcher/Assets/Cinematic/Fade.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Fade : MonoBehaviour {
+    public GameObject fade;
+    public GameObject canvas;
+
+    private IEnumerator Fading()
+    {
+        yield return new WaitForSeconds(10);
+        //fade.SetActive(true);
+        //yield return new WaitForSeconds(.7f);
+        canvas.SetActive(true);
+        fade.SetActive(false);
+        //SceneManager.LoadScene(1);
+    }
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(Fading());
+	}
+}
--- Steve Star Catcher/Assets/Cinematic/Movie.cs
+++ Steve Star Catcher/Assets/Cinematic/Movie.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class Movie : MonoBehaviour {
 
-    //public MovieTexture movTexture;
+    //public GameObject movTexture;
     //void Start()
     //{
     //    GetComponent<Renderer>().material.mainTexture = movTexture;
@@ -16,13 +16,33 @@ public class Movie : MonoBehaviour {
     //    ((MovieTexture)GetComponent<Renderer>().material.mainTexture).Play();
     //}
 
+    public float minimum = 0.0f;
+    public float maximum = 1f;
+    public float duration = 5.0f;
+    private float startTime;
+    private SpriteRenderer sprite;
+
+
     void Start()
     {
         MovieTexture movie = GetComponent<Renderer>().material.mainTexture as MovieTexture;
         movie.Play();
 
-        
+        //Renderer rend = GetComponent<Renderer>();
+        //rend.material.shader = Shader.Find("Specular");
+        //rend.material.SetColor("_SpecColor", Color.red);
+
+        sprite = GetComponent<SpriteRenderer>();
+
+        sprite.color = new Color(0, 0, 0);
+
+        //startTime = Time.time;
     }
 
+    //void Update()
+    //{
+    //    float t = (Time.time - startTime) / duration;
+    //    sprite.color = new Color(1f, 1f, 1f, Mathf.SmoothStep(minimum, maximum, t));
+    //}
 
 }
