30771b6879a1d2779723fe10a96130f168f6b384
Ben <Fuzzinator12@gmail.com>
2016-11-21 16:22:08 -0700

    player characteristics randomly increased

--- 2670/Assets/Player.cs
+++ 2670/Assets/Player.cs
@@ -16,6 +16,10 @@ public class Player : HumanBase {
 
         }
 
+        strength = characteristics[0];
+        perception = characteristics[1];
+        intelligence = characteristics[2];
+        dexterity = characteristics[3];
 
         print("Your characters base stats are: " + strength + " " + perception + " " + intelligence + " " + dexterity);
         
