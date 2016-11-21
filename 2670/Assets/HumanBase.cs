c774379908706cf246567bae990aab053cba5d5e
Ben <Fuzzinator12@gmail.com>
2016-11-21 15:27:59 -0700

    updates to 2670 game

--- 2670/Assets/HumanBase.cs
+++ 2670/Assets/HumanBase.cs
@@ -4,19 +4,30 @@ using System;
 
 public class HumanBase : MonoBehaviour, IAttack, IDamage {
 
-    public void Attack()
+    public int strength;
+    public int perception;
+    public int intelligence;
+    public int dexterity;
+
+
+
+    public virtual void Attack()
     {
         throw new NotImplementedException();
     }
 
-    public void Damage()
+    public virtual void Damage()
     {
         throw new NotImplementedException();
     }
 
     // Use this for initialization
     public virtual void Start () {
-	
+        strength = 5;
+        perception = 5;
+        intelligence = 5;
+        dexterity = 5;
+
 	}
 	
 	// Update is called once per frame
--- 2670/Assets/Player.cs
+++ 2670/Assets/Player.cs
@@ -6,7 +6,12 @@ public class Player : HumanBase {
 	// Use this for initialization
 	public override void Start () {
         base.Start();
+
+        print("Your characters base stats are: " + strength + " " + perception + " " + intelligence + " " + dexterity);
+
         print("do other stuff");
+        
+
 	}
 	
 	// Update is called once per frame
