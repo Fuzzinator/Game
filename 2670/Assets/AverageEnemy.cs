4fcf691b751497d7dbe3696602043b23c3b18dfc
Ben <Fuzzinator12@gmail.com>
2016-11-22 22:15:35 -0700

    new for statement and plain texture added

--- 2670/Assets/AverageEnemy.cs
+++ 2670/Assets/AverageEnemy.cs
@@ -3,17 +3,34 @@ using System.Collections;
 
 public class AverageEnemy : HumanBase {
 
-    
+    private int scoreIncrease;
+    private int charCounter;
 
     // Use this for initialization
     public override void Start()
     {
         base.Start();
 
+        for (int i = 0; i < StaticVars.addScore; i++)
+        {
+            charCounter = UnityEngine.Random.Range(0, 4);
+            scoreIncrease = UnityEngine.Random.Range(-1, 1);
+            characteristics[charCounter] += scoreIncrease;
+
+            if (characteristics[scoreIncrease] > 10)
+            {
+                characteristics[i]--;
+                StaticVars.addScore++;
+            }
+
+        }
+
+
     }
 
     public override sealed void Attack()
     {
         this.GetComponent<EnemyAttack>().Attack();
+
     }
 }
